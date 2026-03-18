e38955595fbafe93b1b73fefb163fe4c4525cc34
moulya2630 <moulyahanisha@gmail.com>
2026-03-18 10:28:55 +0530

    fixed script

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
-button {
-  padding: 10px;
-  font-size: 16px;
-}
+cconst button = document.querySelector("button");
+const container = document.querySelector("#news");
 
-p {
-  background: #f2f2f2;
-  margin: 10px;
-  padding: 10px;
-  border-radius: 8px;
+button.addEventListener("click", getNews);
+
+async function getNews() {
+  container.innerHTML = "Loading...";
+
+  try {
+    const response = await fetch(
+      "https://newsapi.org/v2/top-headlines?country=in&apiKey=14458480cd214f44b9cd3b412f35d468"
+    );
+
+    const data = await response.json();
+
+    container.innerHTML = "";
+
+    data.articles.forEach(article => {
+      const p = document.createElement("p");
+      p.textContent = article.title;
+      container.appendChild(p);
+    });
+
+  } catch (error) {
+    container.innerHTML = "Error fetching news";
+    console.log(error);
+  }
+}
+  } catch (error) {
+    container.innerHTML = "Error fetching news";
+    console.log(error);
+  }
 }
